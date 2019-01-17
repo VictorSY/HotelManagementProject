45ceb6b05e939caa113aad6a7275baa0083373a8
rcvanhollebeke <36461549+rcvanhollebeke@users.noreply.github.com>
2019-01-17 13:27:22 -0800

    Create Room.java

---
+++ Room.java
@@ -0,0 +1,9 @@
+public class Room {
+  private int roomNum;
+  private int roomSize;
+  private String bedSize;
+  private int bedNum;
+  private double cost;
+  private Guest guest;
+  private boolean allowsPets;
+}
