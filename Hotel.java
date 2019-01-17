5b7875bf1eee51b7ceb2c9076f5f16ebcad5cbb8
Victor S <lninjavs@gmail.com>
2019-01-17 14:05:24 -0800

    Added method headers to Hotel class.

--- Hotel.java
+++ Hotel.java
@@ -1,16 +1,33 @@
+
+import java.util.*;
+
 public class Hotel {
 
-    private int numberOfFloors = 2, numberOfRooms = 3;
+    private int numberOfFloors = 2;
+    private int numberOfRooms = 3;
 
     private Array[] floors = new Array[numberOfFloors];
     private Room[] floor = new Array[numberOfRooms];
 
     public static void main(String[] args) {
-
+        createHotelFloorsAndRooms();
     }
 
-    public void createHotelFloorsAndRooms() {}
-    public Guest createGuest() { return new Guest(); }
+    // Creates rooms on each floor with default constructor
+    public void createHotelFloorsAndRooms() {
+        for (Room[] floor : floors) {
+            for (int i = 0; i < Array.length; i++) {
+                floor[i] = new Room[];
+            }
+        }
+    }
+
+    // Uses scanner to prompt for guest info
+    public Guest createGuest() {
+        return new Guest();
+    }
+
+    //
     public Room[] createFloor() { return new Room[]; }
     public double totalCost(Guest guest, int roomNumber) {}
     public void makeReservation(Guest guest, int roomNumber) {}
