f43ee533f4d029b3ce5badf1d1ea9b9fa2dea1b8
rcvanhollebeke <36461549+rcvanhollebeke@users.noreply.github.com>
2019-01-21 13:19:10 -0800

    Update Guest.java
    Created a potential option for the costOfGuests method. Also added the simple checkIn and checkOut methods. Added a Room field to the class. And lastly added the toString method.

--- Guest.java
+++ Guest.java
@@ -16,60 +16,83 @@ public class Guest{
   
   private String bedType;
   private int bedNum;
+  // added Room object to facilitate the integration of the class
+  private Room room;
   
   public Guest(){
-      this.isMembership = false;
-      this.isMilitary = false;
-      this.isGovernment = false;
-      this.numOfSeniors = 0;
-      this.numOfAdults = 0;
-      this.numOfChildren = 0;
-      this.hasPets = false;
-      
-      this.name = "";
-      this.cardNum = 0;
-      
-      this.bedType = "";
-      this.bedNum = 0;
+    this.isMembership = false;
+    this.isMilitary = false;
+    this.isGovernment = false;
+    this.numOfSeniors = 0;
+    this.numOfAdults = 0;
+    this.numOfChildren = 0;
+    this.hasPets = false;
+    
+    this.name = "";
+    this.cardNum = 0;
+    
+    this.bedType = "";
+    this.bedNum = 0;
   }  
   
   public Guest(boolean isMembership, boolean isMilitary, boolean isGovernment,
-                int numOfSeniors, int numOfAdults, int numOfChildren,
-                boolean hasPets, String name, int cardNum, String bedType
-                , int bedNum){
-      this.isMembership = isMembership;
-      this.isMilitary = isMilitary;
-      this.isGovernment = isGovernment;
-      this.numOfSeniors = numOfSeniors;
-      this.numOfAdults = numOfAdults;
-      this.numOfChildren = numOfChildren;
-      this.hasPets = hasPets;
-      
-      this.name = name;
-      this.cardNum = cardNum;
-      
-      this.bedType = bedType;
-      this.bedNum = bedNum;
-                
-    }
+               int numOfSeniors, int numOfAdults, int numOfChildren,
+               boolean hasPets, String name, int cardNum, String bedType
+                 , int bedNum){
+    this.isMembership = isMembership;
+    this.isMilitary = isMilitary;
+    this.isGovernment = isGovernment;
+    this.numOfSeniors = numOfSeniors;
+    this.numOfAdults = numOfAdults;
+    this.numOfChildren = numOfChildren;
+    this.hasPets = hasPets;
+    
+    this.name = name;
+    this.cardNum = cardNum;
+    
+    this.bedType = bedType;
+    this.bedNum = bedNum;
+    
+  }
   
   public double costOfGuests(){
-      // calculate the total cost of the Guest
-      // I haven't use basicPricePerPerson yet for the cost
-      
-      double total = (numOfSeniors*50 + numOfAdults*100 + numOfChildren*30);
-      if(isMembership == true || isMilitary == true || isGovernment == true){
-          total *= 0.8;
-        }         
-      return total;
+    // calculate the total cost of the Guest
+    // I haven't use basicPricePerPerson yet for the cost
+    
+    // possible other option with reasonable additional charges for cost of guests, would be added to the cost of room
+    //double total = (numOfSeniors*4.99 + numOfAdults*9.99 + numOfChildren*3.99);
+    //if(isMembership == true || isMilitary == true || isGovernment == true){
+      //total *= 0.8;
+    //}         
+    //return total;
+  //}
+    
+    double total = (numOfSeniors*50 + numOfAdults*100 + numOfChildren*30);
+    if(isMembership == true || isMilitary == true || isGovernment == true){
+      total *= 0.8;
+    }         
+    return total;
   }
   
   public void checkIn(){
     // Guest check in (use Room class)
+    room.setGuest(this);
   }
   
   public void checkOut(){
     // Guest check out (use Room class)
+    room.removeGuest();
   }
-
+  
+  // the toString method
+  public String toString() {
+    return "Membership: " + isMembership +
+      "\nMilitary Discount: " + isMilitary +
+      "\nGovernment Discount: " + isGovernment +
+      "\nHas Pets: " + hasPets +
+      "\nNumber of Seniors: " + numOfSeniors +
+      "\nNumber of Adults: " + numOfAdults +
+      "\nNumber of Children: " + numOfChildren;
+  }
+  
 }
