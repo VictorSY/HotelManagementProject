c3181cb13aaffa5e8b5b35a109efd4cd3c120ac1
fabrizio-fp <46854996+fabrizio-fp@users.noreply.github.com>
2019-02-01 02:47:45 -0800

    Starting here for debugging

--- Main.java
+++ Main.java
@@ -15,41 +15,44 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         // Scanner for asking questions
         Scanner console = new Scanner(System.in);
-        // allows for the creation of hotels
-        ArrayList<Hotel> hotels = new ArrayList<Hotel>();
-        // adds the hotel for the .txt sample file
-        hotels.add(new Hotel("SampleHotelData.txt", console));
-        // loops through the command-line prompts
-        hotels.get(0).createGuest();
-        while(true) {
-            if(Guest.yesOrNoQuestions("Would you like to cancel a reservation? " +
-                    "If so type \"yes\" if not type \"no\"\n", new Scanner(System.in))) {
-                System.out.println("Enter your full name (exactly as you made your reservation as): ");
-                String name = console.nextLine().toLowerCase().replaceAll("[^a-z]", " ");
-                try {
-                    Guest guest = hotels.get(0).findGuestInList(name);
-                    hotels.get(0).cancelReservation(guest);
-                    System.out.println("Successfully checked out.");
-                } catch(IllegalArgumentException e) {
-                    if(Guest.yesOrNoQuestions("Guest not found. Try again?", console)) {
-                        continue;
-                    }
-                }
-            }
+        // // allows for the creation of hotels
+        // ArrayList<Hotel> hotels = new ArrayList<Hotel>();
+        // // adds the hotel for the .txt sample file
+        
+        Hotel hotels = new Hotel("SampleHotelData.txt", console);
+        hotels.createGuest();
+        hotels.makeReservation(hotels.getGuestList().get(0), hotels.floors.get(0).get(0), console);
+        
+        // // loops through the command-line prompts
+        // while(true) {
+            // if(Guest.yesOrNoQuestions("Would you like to cancel a reservation? " +
+                    // "If so type \"yes\" if not type \"no\"\n", new Scanner(System.in))) {
+                // System.out.println("Enter your full name (exactly as you made your reservation as): ");
+                // String name = console.nextLine().toLowerCase().replaceAll("[^a-z]", " ");
+                // try {
+                    // Guest guest = hotels.get(0).findGuestInList(name);
+                    // hotels.get(0).cancelReservation(guest);
+                    // System.out.println("Successfully checked out.");
+                // } catch(IllegalArgumentException e) {
+                    // if(Guest.yesOrNoQuestions("Guest not found. Try again?", console)) {
+                        // continue;
+                    // }
+                // }
+            // }
 
-            // Ends the program if user wants to
-            if(Guest.yesOrNoQuestions("Would you like to end the program? " +
-                    "If so type \"yes\" if not type \"no\"\n", console)) {
-                System.out.println("Thank you. Goodbye.");
-                System.exit(0);
-            }
+            // // Ends the program if user wants to
+            // if(Guest.yesOrNoQuestions("Would you like to end the program? " +
+                    // "If so type \"yes\" if not type \"no\"\n", console)) {
+                // System.out.println("Thank you. Goodbye.");
+                // System.exit(0);
+            // }
 
-            // creates guests to run the program
-            hotels.get(0).createGuest();
+            // // creates guests to run the program
+            // hotels.get(0).createGuest();
 
 
-            // creates neat formatting
-            System.out.println("\n\n\n\n");
-        }
+            // // creates neat formatting
+            // System.out.println("\n\n\n\n");
+        // }
     }
 }
